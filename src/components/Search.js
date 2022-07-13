932d77fb3986833ab738b1f584d5d5809b1f4d36
Ryan Smillie <RyanJSmillie@hotmail.co.uk>
2022-07-13 21:45:16 +0100

    first commit

---
+++ src/components/Search.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState } from "react";
+
+function Search() {
+  return (
+    <label htmlFor="search">
+      <input className="searchInput" type="text" placeholder="Search" />
+    </label>
+  );
+}
+
+export default Search;
